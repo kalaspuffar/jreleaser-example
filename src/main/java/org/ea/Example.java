a111b398c64c7b274b002daa9cfd4da6dd0eb49d
Daniel Persson <mailto.woden@gmail.com>
2021-08-07 15:06:10 +0200

    Testing package before release.

--- src/main/java/org/ea/Example.java
+++ src/main/java/org/ea/Example.java
@@ -2,6 +2,6 @@ package org.ea;
 
 public class Example {
     public static void main(String[] args) {
-        System.out.println("Hello explorers! Test 2");
+        System.out.println("Hello explorers! Testing package before release");
     }
 }
