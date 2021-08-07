7888e789eb5226a5538594ac061bc22bb9274915
Daniel Persson <mailto.woden@gmail.com>
2021-08-07 15:04:10 +0200

    Testing another commit 2.

--- src/main/java/org/ea/Example.java
+++ src/main/java/org/ea/Example.java
@@ -2,6 +2,6 @@ package org.ea;
 
 public class Example {
     public static void main(String[] args) {
-        System.out.println("Hello explorers! Test 1");
+        System.out.println("Hello explorers! Test 2");
     }
 }
